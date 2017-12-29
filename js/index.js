1e83698c0523a51e04bd40de61e30688f897afed
zxy111z <1627381928@qq.com>
2017-12-29 09:14:39 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Administrator on 2017/12/28/028.
+ */
+{
+    // 轮播图
+    let imgs=document.querySelectorAll(".banner-center li");
+    let pages=document.querySelectorAll(".lunbo-box ul li");
+    pages.forEach(function(value,index){
+        value.onclick=function(){
+            for(var i=0;i<imgs.length;i++){
+                imgs[i].classList.remove("active");
+                pages[i].classList.remove("active");
+            }
+            this.classList.add("active");
+            imgs[index].classList.add("active");
+            n=index;
+        }
+    })
+    // 自动轮播
+    let n=0;
+    let banner=document.querySelector(".banner");
+    function bannerfn(dir="r"){
+        if(dir==="r"){
+            n++;
+            if(n===imgs.length){
+                n=0;
+            }
+        }else if(dir==="l"){
+           n--;
+           if(n===-1){
+               n=imgs.length-1;
+           }
+       }
+        for(var i=0;i<imgs.length;i++){
+            imgs[i].classList.remove("active");
+            pages[i].classList.remove("active");
+        }
+        imgs[n].classList.add("active");
+        pages[n].classList.add("active");
+    }
+    let t=setInterval(bannerfn,3000);
+    banner.onmouseover=function(){
+        clearInterval(t);
+    }
+    banner.onmouseout=function(){
+        t=setInterval(bannerfn,3000);
+    }
+    // 左右箭头
+    let left=document.querySelector(".lunbo-left");
+    let right=document.querySelector(".lunbo-right");
+    let flag=true;
+    right.onclick=function(){
+        if(flag){
+            flag=false;
+            bannerfn();
+        }
+    }
+    left.onclick=function(){
+        if(flag){
+            flag=false;
+            bannerfn("l");
+        }
+    }
+    imgs.forEach(function(val,index){
+        val.addEventListener("transitionend",function(){
+            flag=true;
+        });
+    })
+}
